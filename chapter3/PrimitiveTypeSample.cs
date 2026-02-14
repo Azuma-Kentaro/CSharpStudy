093ae69dc29a3f5f30d9850009b111059159286e
kentaro Azuma <azuma.199x@gmail.com>
2026-02-14 09:55:28 +0900

    Change: Remove Redundant string

--- chapter3/PrimitiveTypeSample.cs
+++ chapter3/PrimitiveTypeSample.cs
@@ -11,16 +11,8 @@ public class PrimitiveTypeSample
 
         CharSample.CharLiteralSample();
 
-        Console.WriteLine("=== start string literal sample ===");
         StringSample.StringLiteralSample();
-        Console.WriteLine("=== end string literal sample ===");
-
-        Console.WriteLine("=== start raw string literal sample ===");
         StringSample.RawStringLiteralSample();
-        Console.WriteLine("=== end raw string literal sample ===");
-
-        Console.WriteLine("=== start UTF-8 literal sample ===");
         StringSample.UTF8LiteralSample();
-        Console.WriteLine("=== end UTF-8 literal sample ===");
     }
 }
